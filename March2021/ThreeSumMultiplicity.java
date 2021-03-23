3f993222e3cb82bb8cb02818e6d1cfd8791a06f9
Rajath Ganesh <rajath2774@gmail.com>
2021-03-23 09:06:47 -0400

    Added 3/23 challenge

---
+++ March2021/ThreeSumMultiplicity.java
@@ -0,0 +1,24 @@
+package March2021;
+
+public class ThreeSumMultiplicity {
+    public static void main(String[] args) {
+        System.out.println("Number of 3sum:" + threeSumMulti(new int[] {1,1,2,2,3,3,4,4,5,5}, 8));
+    }
+    public static int threeSumMulti(int[] A, int target) {
+        long[] c = new long[101];
+        for (int a : A) c[a]++;
+        long res = 0;
+        for (int i = 0; i <= 100; i++)
+            for (int j = i; j <= 100; j++) {
+                int k = target - i - j;
+                if (k > 100 || k < 0) continue;
+                if (i == j && j == k)
+                    res += c[i] * (c[i] - 1) * (c[i] - 2) / 6;
+                else if (i == j)
+                    res += c[i] * (c[i] - 1) / 2 * c[k];
+                else if (j < k)
+                    res += c[i] * c[j] * c[k];
+            }
+        return (int)(res % (1e9 + 7));
+    }
+}
