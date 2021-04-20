639bfa9d12b6ac4025e29f571a1dd4b45712f03e
Rajath Ganesh <rajath2774@gmail.com>
2021-04-20 08:51:23 -0400

    Added 4/20 challenge

---
+++ April2021/PreOrderNary.java
@@ -0,0 +1,38 @@
+package April2021;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+public class PreOrderNary {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return  res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node node = stack.pop();
+            res.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.push(node.children.get(i));
+            }
+        }
+        return res;
+    }
+}
